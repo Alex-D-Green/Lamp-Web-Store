7ee92ebf674f379231000b2bdfe6277f31e9f0c8
Alex D Green <das2009@km.ru>
2019-11-01 22:52:35 +0300

    All CRUD operations were implemented.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -92,6 +92,120 @@ namespace LampWebStore.Controllers
         }
 
         /// <summary>
+        /// Get method, just show the empty form.
+        /// </summary>
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Post method, check if lamp's data is ok and add new entity into the DB.
+        /// </summary>
+        [HttpPost]
+        [ValidateAntiForgeryToken] //To prevent handling Post requests from the foreign apps
+        public async Task<IActionResult> Create(LampViewModel lamp)
+        {
+            if(!ModelState.IsValid)
+                return View(lamp); //There are some validation errors, reshow form
+
+            //Here could be used AutoMapper or something like this...
+            db.Lamps.Add(new Lamp {
+                //lamp.Id is ignored here
+                Cost = lamp.Cost,
+                ImageRef = lamp.ImageRef,
+                LampType = lamp.LampType,
+                Manufacturer = lamp.Manufacturer
+            });
+
+            await db.SaveChangesAsync(); //DB/ORM exceptions probably should being handled in a special way...
+
+            return RedirectToAction("Products");
+        }
+
+        /// <summary>
+        /// Show the deletion confirmation form.
+        /// </summary>
+        [ActionName("Delete")] //To use same name for both Get and Post requests
+        public async Task<ActionResult> ConfirmDelete(int id)
+        {
+            if(id <= 0)
+                return BadRequest($"Inappropriate id ({id}).");
+
+            Lamp item = await db.Lamps.FirstOrDefaultAsync(o => o.Id == id);
+
+            if(item is null)
+                return BadRequest($"Item with id = {id} was not found.");
+
+            return View(item);
+        }
+
+        /// <summary>
+        /// Delete the item.
+        /// </summary>
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id)
+        {
+            Lamp item = await db.Lamps.FirstOrDefaultAsync(o => o.Id == id);
+
+            if(item is null)
+                return BadRequest($"Item with id = {id} was not found.");
+
+            db.Remove(item);
+            await db.SaveChangesAsync();
+
+            return RedirectToAction("Products");
+        }
+
+        /// <summary>
+        /// Get method, just show the form with data.
+        /// </summary>
+        public async Task<IActionResult> Edit(int id)
+        {
+            if(id <= 0)
+                return BadRequest($"Inappropriate id ({id}).");
+
+            Lamp item = await db.Lamps.FirstOrDefaultAsync(o => o.Id == id);
+
+            if(item is null)
+                return BadRequest($"Item with id = {id} was not found.");
+
+            //Here could be used AutoMapper or something like this...
+            return View(new LampViewModel {
+                Id = item.Id,
+                Cost = item.Cost,
+                ImageRef = item.ImageRef,
+                LampType = item.LampType,
+                Manufacturer = item.Manufacturer
+            });
+        }
+
+        /// <summary>
+        /// Post method, check if lamp's data is ok and change entity into the DB.
+        /// </summary>
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(LampViewModel lamp)
+        {
+            if(!ModelState.IsValid)
+                return View(lamp); //There are some validation errors, reshow form
+
+            Lamp item = await db.Lamps.FirstOrDefaultAsync(o => o.Id == lamp.Id);
+
+            if(item is null)
+                return BadRequest($"Item with id = {lamp.Id} was not found.");
+
+            item.Cost = lamp.Cost;
+            item.ImageRef = lamp.ImageRef;
+            item.LampType = lamp.LampType;
+            item.Manufacturer = lamp.Manufacturer;
+
+            db.Lamps.Update(item);
+            await db.SaveChangesAsync(); //DB/ORM exceptions probably should being handled in a special way...
+
+            return RedirectToAction("Products");
+        }
+
+        /// <summary>
         /// Show some error's info but without sensitive information like stack trace etc.
         /// </summary>
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Views/ViewModels/LampViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+using LampWebStore.Models;
+
+
+namespace LampWebStore.Views.ViewModels
+{
+    /// <summary>
+    /// Lamp view model that used when need to add new lamp entity or change the existing one.
+    /// </summary>
+    public class LampViewModel
+    {
+        //I use the same view model for adding and editing actions although the Id field is redundant 
+        //for adding. It's probably better to have two different view models for these actions.
+        public int Id { get; set; }
+
+        [DisplayName("Lamp type")]
+        public LampType LampType { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Manufacturer")]
+        public string Manufacturer { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Range(0, 2000)]
+        [DisplayName("Cost")]
+        public double Cost { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        [StringLength(500)]
+        [DisplayName("Image link")]
+        public string ImageRef { get; set; }
+    }
+}
